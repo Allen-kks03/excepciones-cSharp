f514e38cd2c5fd7a88a03dd88bfbcdd413713da6
Allen Jacob Suarez Briseño <l21212056@tectijuana.edu.mx>
2022-05-19 11:44:02 -0700

    excepciones-cSharp
    Clase 19 de mayo 2022

---
+++ excepciones-cSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excepciones_cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Dame un numero: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Dame el otro numero: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num1 / num2);
+                Console.ReadLine();
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
